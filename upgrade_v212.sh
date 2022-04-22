cdd2e57188c6493ebaf320cf5aba252ccee57572
armaanPy <52916251+armaanPy@users.noreply.github.com>
2022-04-22 15:46:44 +0100

    Create upgrade_v212.sh

---
+++ upgrade_v212.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Scipt which is used to determine the upgrade height of 922,363, at which users will see the following error message on the chain-maind: ERR UPGRADE "v2.0.0" NEEDED
+
+LOG=~/node.log
+ERROR='ERR UPGRADE "v2.0.0" NEEDED'
+
+while true; do
+        if grep -q "$ERROR" "$LOG"; then
+                exit
+        else
+                sleep 60
+                continue
+        fi
+        break
+done
