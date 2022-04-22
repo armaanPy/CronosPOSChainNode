f4d868fc4c085716493b68ed61cb03ae5a61c687
armaanPy <52916251+armaanPy@users.noreply.github.com>
2022-04-22 19:37:05 +0100

    Update upgrade_v332.sh

--- upgrade_v332.sh
+++ upgrade_v332.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Scipt which is used to determine the upgrade height of 3,526,800, at which users will see the following error message on the chain-maind: ERR UPGRADE "v3.0.0" NEEDED
 
-
 LOG=~/node.log
 ERROR='ERR UPGRADE "v3.0.0" NEEDED'
 
