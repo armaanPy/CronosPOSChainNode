14d93845804868290523edcb51475af656432cda
armaanPy <52916251+armaanPy@users.noreply.github.com>
2023-08-02 11:51:59 +0100

    Update upgrade_v332.sh

--- upgrade_v332.sh
+++ upgrade_v332.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Scipt which is used to determine the upgrade height of 3,526,800, at which users will see the following error message on the chain-maind: ERR UPGRADE "v3.0.0" NEEDED
+# Script which is used to determine the upgrade height of 3,526,800, at which users will see the following error message on the chain-maind: ERR UPGRADE "v3.0.0" NEEDED
 
 LOG=~/node.log
 ERROR='ERR UPGRADE "v3.0.0" NEEDED'
